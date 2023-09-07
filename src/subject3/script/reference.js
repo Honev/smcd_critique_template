55046cb141b6a268a70e51ba5f50904a43098d3d
Honev <132217078+Honev@users.noreply.github.com>
2023-09-07 14:44:18 +0900

    인터렉션레퍼런스 추가

--- src/subject3/script/reference.js
+++ src/subject3/script/reference.js
@@ -1,7 +1,7 @@
 const reference = [
   {
     author: 'Download Page',
-    title: '',
+    title: 'Download Page',
     year: '',
     sourceTitle: '',
     link: 'https://www.figma.com/proto/zIsssMv6NMifue7se48IzH/IDP-231?page-id=449%3A723&type=design&node-id=1329-6238&viewport=-2972%2C423%2C0.07&t=yssx5m0XXC2rhtBK-1&scaling=scale-down&starting-point-node-id=728%3A1282&mode=design',
