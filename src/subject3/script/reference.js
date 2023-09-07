7970cef3fa4ff668b4a3c10e505456417f0ffd93
Honev <132217078+Honev@users.noreply.github.com>
2023-09-07 14:39:48 +0900

    피그마 링크 추가

--- src/subject3/script/reference.js
+++ src/subject3/script/reference.js
@@ -1,9 +1,9 @@
 const reference = [
   {
-    author: "",
-    title: "",
-    year: "",
-    sourceTitle: "",
-    link: "",
+    author: 'Download Page',
+    title: '',
+    year: '',
+    sourceTitle: '',
+    link: 'https://www.figma.com/proto/zIsssMv6NMifue7se48IzH/IDP-231?page-id=449%3A723&type=design&node-id=1329-6238&viewport=-2972%2C423%2C0.07&t=yssx5m0XXC2rhtBK-1&scaling=scale-down&starting-point-node-id=728%3A1282&mode=design',
   },
 ];
