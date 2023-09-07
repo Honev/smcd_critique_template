743d6c5d5379c28bcafa0627f5772faaf41ec457
Honev <132217078+Honev@users.noreply.github.com>
2023-09-07 15:23:24 +0900

    피그마 링크수정

--- src/subject3/script/reference.js
+++ src/subject3/script/reference.js
@@ -4,6 +4,6 @@ const reference = [
     title: 'Download Page',
     year: 'DEMO',
     sourceTitle: '',
-    link: 'https://www.figma.com/proto/zIsssMv6NMifue7se48IzH/IDP-231?page-id=449%3A723&type=design&node-id=1329-6238&viewport=-2972%2C423%2C0.07&t=yssx5m0XXC2rhtBK-1&scaling=scale-down&starting-point-node-id=728%3A1282&mode=design',
+    link: 'https://www.figma.com/proto/zIsssMv6NMifue7se48IzH/IDP-231?page-id=133%3A351&type=design&node-id=1438-6281&viewport=-197%2C2634%2C0.11&t=iY6zHAJtk0vxxwqO-1&scaling=scale-down&mode=design',
   },
 ];
