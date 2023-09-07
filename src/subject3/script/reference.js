57b77ff89a7cc7d0f2bc3d050f69ec4ee5104046
Honev <132217078+Honev@users.noreply.github.com>
2023-09-07 15:09:32 +0900

    인터렉션 래퍼런스 수정

--- src/subject3/script/reference.js
+++ src/subject3/script/reference.js
@@ -1,8 +1,8 @@
 const reference = [
   {
-    author: '나 혼자만 지적재산권 마음대로 사용하기 DEMO',
+    author: '나 혼자만 지적재산권 마음대로 사용하기',
     title: 'Download Page',
-    year: '',
+    year: 'DEMO',
     sourceTitle: '',
     link: 'https://www.figma.com/proto/zIsssMv6NMifue7se48IzH/IDP-231?page-id=449%3A723&type=design&node-id=1329-6238&viewport=-2972%2C423%2C0.07&t=yssx5m0XXC2rhtBK-1&scaling=scale-down&starting-point-node-id=728%3A1282&mode=design',
   },
