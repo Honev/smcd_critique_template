c51491787b0523774a769aae7242817f93a3cc51
Honev <132217078+Honev@users.noreply.github.com>
2023-09-07 16:01:49 +0900

    게임링크로 변경

--- src/subject3/script/reference.js
+++ src/subject3/script/reference.js
@@ -1,9 +1,9 @@
 const reference = [
   {
     author: '나 혼자만 지적재산권 마음대로 사용하기',
-    title: 'Download Page',
+    title: 'Download Link',
     year: 'DEMO',
     sourceTitle: '',
-    link: 'https://www.figma.com/proto/1aLP4DUzBy9OXeViJeLxbz/Game?page-id=0%3A1&type=design&node-id=1-14&viewport=574%2C482%2C0.12&t=1G2koto8jgivNuzJ-1&scaling=min-zoom&mode=design',
+    link: 'https://honev.github.io/Game/',
   },
 ];
